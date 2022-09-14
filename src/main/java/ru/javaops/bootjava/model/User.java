85dc5a5b8dbefd3d6db7060ccb0ab6b842d587b8
vazh <jobvalerii@gmail.com>
2022-09-14 23:20:17 +0500

    2_02_h2_init

--- src/main/java/ru/javaops/bootjava/model/User.java
+++ src/main/java/ru/javaops/bootjava/model/User.java
@@ -3,6 +3,10 @@ package ru.javaops.bootjava.model;
 import lombok.*;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.Set;
 
 @Entity
